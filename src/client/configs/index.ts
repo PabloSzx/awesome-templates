4dbaa489ea3b77836e7624926168dcffd833c4b2
PabloSz <pablosaez1995@gmail.com>
2019-08-11 21:00:22 -0400

    fix graphql url

--- src/client/configs/index.ts
+++ src/client/configs/index.ts
@@ -1,1 +1,4 @@
-export const GRAPHQL_URL = "http://localhost:3000/api/graphql";
+export const GRAPHQL_URL =
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:3000/api/graphql"
+    : "/api/graphql";
