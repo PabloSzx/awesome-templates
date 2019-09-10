d108670686b928c97fc15ebcf7799331d2aaf232
PabloSz <pablosaez1995@gmail.com>
2019-09-10 13:24:32 -0300

    clean

--- src/client/configs/index.ts
+++ src/client/configs/index.ts
@@ -1,3 +1,4 @@
-export const GRAPHQL_URL = process.env.NODE_ENV === "production" 
-  ? "https://awesome-templates.dev/api/graphql"
-  : "http://localhost:3000/api/graphql";
+export const GRAPHQL_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://awesome-templates.dev/api/graphql"
+    : "http://localhost:3000/api/graphql";
--- src/server/consts/index.ts
+++ src/server/consts/index.ts
@@ -2,9 +2,10 @@ export const ADMIN = "admin";
 export const APP_INSTALLED = "app_installed";
 export const WRONG_INFO = "WRONG_INFO";
 export const USER_ALREADY_EXISTS = "USER_ALREADY_EXISTS";
-export const LOCAL_PATH = process.env.NODE_ENV === "production" 
-  ? "https://awesome-templates.dev/"
-  : "http://localhost:3000/";
+export const LOCAL_PATH =
+  process.env.NODE_ENV === "production"
+    ? "https://awesome-templates.dev/"
+    : "http://localhost:3000/";
 export enum APILevel {
   BASIC = "BASIC",
   MEDIUM = "MEDIUM",
