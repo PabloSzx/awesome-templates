9c94dad4e3e676afed4464e7f258b2402ebb6320
Pablo Saez <pablosaez1995@gmail.com>
2019-09-10 12:58:24 -0300

    Fix graphql url

--- src/server/consts/index.ts
+++ src/server/consts/index.ts
@@ -2,7 +2,7 @@ export const ADMIN = "admin";
 export const APP_INSTALLED = "app_installed";
 export const WRONG_INFO = "WRONG_INFO";
 export const USER_ALREADY_EXISTS = "USER_ALREADY_EXISTS";
-export const LOCAL_PATH = process.env.NOW_REGION
+export const LOCAL_PATH = process.env.NODE_ENV === "production" 
   ? "https://awesome-templates.dev/"
   : "http://localhost:3000/";
 export enum APILevel {
