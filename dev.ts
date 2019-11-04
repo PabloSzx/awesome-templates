d8f71f83d7c759ff3d5a924524eaaedf2b611f0e
PabloSzx <pablosaez1995@gmail.com>
2019-11-04 19:33:46 -0300

    lerna implementation, update dependencies, wait-port

--- dev.ts
+++ dev.ts
@@ -1,16 +1,25 @@
 import express from "express";
 import proxy from "http-proxy-middleware";
 import notifier from "node-notifier";
+import waitPort from "wait-port";
 
-const app = express();
+(async () => {
+  const app = express();
 
-app.use("/api", proxy("http://localhost:4000"));
-app.use(proxy("http://localhost:3000"));
+  console.log("Waiting for API and Client servers...");
+  await Promise.all([
+    waitPort({ port: 3000, output: "silent" }),
+    waitPort({ port: 4000, output: "silent" }),
+  ]);
 
-app.listen(8000, () => {
-  console.log("Proxy Server ready at http://localhost:8000");
-  notifier.notify({
-    title: "🚀  Proxy Server ready",
-    message: `at http://localhost:8000`,
+  app.use("/api", proxy("http://localhost:4000"));
+  app.use(proxy("http://localhost:3000"));
+
+  app.listen(8000, () => {
+    console.log("Proxy Server ready at http://localhost:8000");
+    notifier.notify({
+      title: "🚀  Proxy Server ready",
+      message: `at http://localhost:8000`,
+    });
   });
-});
+})();
